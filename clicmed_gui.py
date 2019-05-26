788de496a00ab3e3934054a1d94c13f6aee87c84
Benjamin <beyerle.benjamin@gmail.com>
2019-05-26 14:13:13 +0200

    admin and user panel

--- clicmed_gui.py
+++ clicmed_gui.py
@@ -11,6 +11,7 @@ MYSQL_USER_PWD = 'clicmedUs3r'
 MYSQL_DB = 'ClicMed'
 ICO = os.path.join(sys.path[0], "Resources/ClicMed.gif")
 ICO_ERROR = os.path.join(sys.path[0], "Resources/incorrect2.png")
+FONT = os.path.join(sys.path[0], "Resources/georgia.ttf")
 
 
 # Hashing password for security
@@ -20,43 +21,46 @@ def password_hash(password_input):
     return hashed_password
 
 
+def clear_window(root_frame):
+    for widget in root_frame.winfo_children():
+        widget.destroy()
+
+
 # Window for log in
 def login_window(root_frame):
 
-    login_f = tk.Toplevel()
-    login_f.configure(background='#3c3f41')
-    login_f.tk.call('wm', 'iconphoto', login_f._w, img_ico)
-    login_f.title('Clic Med')
-    login_f.geometry('250x130')
-    login_f.maxsize(250, 130)
-    login_f.minsize(250, 130)
+    root_frame.configure(background='#3c3f41')
+    root_frame.title('Clic Med')
+    root_frame.geometry('250x130')
+    root_frame.maxsize(250, 130)
+    root_frame.minsize(250, 130)
 
-    label1 = tk.Label(login_f, text='Login', font=("Georgia", 30), fg='white', bg='#3c3f41')
-    label1.grid(column=1, columnspan=2, sticky='W')
+    label1 = tk.Label(root_frame, text='Login', font=("Arial", 30), fg='white', bg='#3c3f41')
+    label1.grid(column=1, columnspan=3, sticky='W')
 
-    label2 = tk.Label(login_f, text='Username :', font=("Georgia", 10), fg='white', bg='#3c3f41')
+    label2 = tk.Label(root_frame, text='Username :', font=("Arial", 10), fg='white', bg='#3c3f41')
     label2.grid(row=1, sticky='E')
 
-    label3 = tk.Label(login_f, text='Password :', font=("Georgia", 10), fg='white', bg='#3c3f41')
+    label3 = tk.Label(root_frame, text='Password :', font=("Arial", 10), fg='white', bg='#3c3f41')
     label3.grid(row=2, sticky='E')
 
-    user = tk.Entry(login_f)
+    user = tk.Entry(root_frame, width=15)
     user.grid(row=1, column=1, columnspan=2, sticky='W')
     user.focus()
 
-    password = tk.Entry(login_f, show="*")
+    password = tk.Entry(root_frame, show="*", width=15)
     password.grid(row=2, column=1, columnspan=2, sticky='W')
 
-    login_btn = tk.Button(login_f, text='Login', command=lambda: check_user(user.get(), password.get(), login_f,
+    login_btn = tk.Button(root_frame, text='Login', command=lambda: check_user(user.get(), password.get(), root_frame,
                                                                             password, root_frame))
     login_btn.grid(row=3, column=1, sticky='E')
 
-    exit_btn = tk.Button(login_f, text='Exit', command=root_frame.destroy)
+    exit_btn = tk.Button(root_frame, text='Exit', command=root_frame.destroy)
     exit_btn.grid(row=3, column=2, sticky='W')
 
 
 # Function to verify if user is in the database (username + password)
-def check_user(username, password_input, login_frame, password_label, root_frame):
+def check_user(username, password_input, root_framerame, password_label, root_frame):
 
     hashed_password = password_hash(password_input)
 
@@ -72,25 +76,41 @@ def check_user(username, password_input, login_frame, password_label, root_frame
     try:
         group_id = records[0][2]
     except IndexError:
-        incorrect = tk.Label(login_frame, image=img_error, background='#3c3f41')
+        incorrect = tk.Label(root_frame, image=img_error, background='#3c3f41')
         incorrect.grid(row=1, column=4, sticky='W')
 
         password_label.delete(0, tk.END)
     else:
-        root_frame.deiconify()
-        fail_img = tk.Label(root_frame, image=tk.PhotoImage(ICO))
-        fail_img.grid()
-        login_frame.destroy()
-        return group_id
+        if group_id == 0:
+            admin(root_frame)
+        else:
+            user(root_frame)
 
 
+def admin(root_frame):
+    clear_window(root_frame)
+    root_frame.configure(background='#3c3f41')
+    root_frame.title('Clic Med Admin')
+    root_frame.geometry('450x330')
+    root_frame.maxsize(450, 330)
+    root_frame.minsize(450, 330)
+
+
+def user(root_frame):
+    clear_window(root_frame)
+    root_frame.configure(background='#3c3f41')
+    root_frame.title('Clic Med Admin')
+    root_frame.geometry('450x330')
+    root_frame.maxsize(450, 330)
+    root_frame.minsize(450, 330)
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------
 # Main execution of program
 root = tk.Tk()
 img_ico = tk.PhotoImage(file=ICO)
 img_error = tk.PhotoImage(file=ICO_ERROR)
-root.withdraw()
-if login_window(root) == 1:
-    print('admin')
-else:
-    print('user')
+root.tk.call('wm', 'iconphoto', root._w, img_ico)
+login_window(root)
 root.mainloop()
