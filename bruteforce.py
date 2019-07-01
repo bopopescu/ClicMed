89271795115c44a61ee7c0f1cb4048c3994f5991
Benjamin <beyerle.benjamin@gmail.com>
2019-07-01 21:47:21 +0200

    Bruteforce

--- bruteforce.py
+++ bruteforce.py
@@ -31,15 +31,33 @@ def main_frame(root_frame, username, group):
     dropdown1.grid(row=0, column=1, pady=2, sticky="w")
     dropdown1.config(borderwidth=0)
 
-    filtr_btn = settings.tk.Button(root_frame, text="Apply",
-                                   command=lambda: bruteforcer(filter1_choice.get()[2:-3], username, group))
-    filtr_btn.grid(row=0, column=1, pady=2, sticky="w", padx=100)
+    filtr_btn = settings.tk.Button(root_frame, text="Attack!",
+                                   command=lambda: bruteforcer(filter1_choice.get()[2:-3], progress_bar, label2,
+                                                               label3))
+    filtr_btn.grid(row=0, column=1, pady=2, sticky="e")
+
+    progress_bar = settings.ttk.Progressbar(root_frame, orient='horizontal', length=165)
+    progress_bar.grid(row=1, column=1, sticky='w', pady=2)
+
+    label1 = settings.tk.Label(root_frame, text='Number of try : ', font=("Arial", 10), fg='white', bg='#3c3f41')
+    label1.grid(row=2, column=0, pady=2, sticky="w")
+
+    label2 = settings.tk.Label(root_frame, text='0', font=("Arial", 10), fg='black', bg='white')
+    label2.grid(row=2, column=1, pady=2, sticky="w")
+
+    label3 = settings.tk.Entry(root_frame, text='0', font=("Arial", 10), fg='black', bg='white', width=10)
+
+    return_btn = settings.tk.Button(root_frame, text='Return',
+                                    command=lambda: settings.menu.main_frame(root_frame, username, group))
+    return_btn.grid(row=3, column=1, sticky="se", padx=35, pady=2)
+
+    exit_btn = settings.tk.Button(root_frame, text='Exit', command=root_frame.destroy)
+    exit_btn.grid(row=3, column=1, sticky="se", pady=2)
 
 
-def bruteforcer(username_target, username, group):
+def bruteforcer(username_target, progress_bar, nb_try, found):
 
-    found = False
-
+    password_try = 0
     available_char = settings.string.ascii_letters + settings.string.digits
     cnx = settings.mysql.connect(host=settings.HOST, user=settings.MYSQL_USER, password=settings.MYSQL_USER_PWD,
                                  database=settings.MYSQL_DB)
@@ -48,16 +66,19 @@ def bruteforcer(username_target, username, group):
     hashed_user_password = str(cursor.fetchall()[0])[2:-3]
     cursor.close()
     cnx.close()
+    password_to_attempt = settings.itertools.product(available_char, repeat=8)
 
-    for length in range(8, 9):
-        password_to_attempt = settings.itertools.product(available_char, repeat=length)
+    progress_bar.config(maximum=(8**8), mode='determinate', value=password_try)
 
-        for attempt in password_to_attempt:
-            attempt = ''.join(attempt)
-            hashed_attempt = settings.login.password_hash(attempt)
-            if hashed_attempt == hashed_user_password:
-                found = True
-                break
-
-        if found:
+    for attempt in password_to_attempt:
+        password_try += 1
+        progress_bar['value'] = password_try
+        nb_try.config(text=password_try)
+        nb_try.update()
+        progress_bar.update()
+        attempt = ''.join(attempt)
+        hashed_attempt = settings.login.password_hash(attempt)
+        if hashed_attempt == hashed_user_password:
+            found.insert(0, attempt)
+            found.grid(row=2, column=1, pady=2, sticky="e")
             break
