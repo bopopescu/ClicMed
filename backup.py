82dc1a2c039389dcec8753f6d39c044fb7dc6276
Benjamin <beyerle.benjamin@gmail.com>
2019-07-15 08:43:34 +0200

    Backup

--- backup.py
+++ backup.py
@@ -16,6 +16,6 @@ for i in range(5, 0, -1):
     print(current, future)
     if os.path.isfile(current):
         os.rename(current, future)
-os.rename('Backup.tar.gz', 'Backup.1.tar.gz')
+os.rename('/home/ftp/Backup/Backup.tar.gz', '/home/ftp/Backup/Backup.1.tar.gz')
 os.system('tar -C /home/ftp/ -cvf /home/ftp/Backup/Backup.tar.gz ClicMed')
 log('System', ' did a backup')
